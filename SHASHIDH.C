55fd0a9efe46889e080eb30b2b34ffdc61ef1619
Shashidharpai <53755231+Shashidharpai@users.noreply.github.com>
2019-08-05 15:55:37 +0530

    Add files via upload

---
+++ SHASHIDH.C
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<conio.h>
+struct student
+{
+int ds;
+int ld;
+int mp;
+int java;
+int coa;
+}s1;
+void main()
+{
+int i=0;
+arr[6];
+clrscr();
+printf("enter the marks of DS LD MP JAVA COA");
+scanf("%d %d %d %d %d",&s1.ds,&s1.ld,&s1.mp,&s1.java,&s1.coa);
+printf("marks of DS: %d,LD: %d,MP: %d,JAVA: %d,COA: %d",s1.ds,s1.ld,s1.mp,s1.java,s1.coa);
+arr[0]=s1.ds;
+arr[1]=s1.ld;
+arr[2]=s1.mp;
+arr[3]=s1.java;
+arr[4]=s1.coa;
+for(i=0;i<6;i++)
+{if(89<arr[i]<=100)
+ arr[i]=10;
+ else if(79<arr[i]<90)
+ arr[i]=9;
+ else if(69<arr[i]<80)
+ arr[i]=8;
+ else if(59<arr[i]<70)
+ arr[i]=7;
+ else if(49<arr[i]<60)
+ arr[i]=6;
+ else if(39<arr[i]<50)
+ arr[i]=5;
+ else
+ arr[i]=0;
+ }
+ printf
+
+
+
+getch();
+}
